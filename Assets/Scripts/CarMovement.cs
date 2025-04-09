e894da716b02baffc5fb5ca4138fc09f89d3d29a
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-04-08 23:24:37 -0400

    Added gas & rotation

--- Assets/Scripts/CarMovement.cs
+++ Assets/Scripts/CarMovement.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class CarMovement : MonoBehaviour
 {
+    [SerializeField] InputAction gas;
+    [SerializeField] InputAction left;
+    [SerializeField] InputAction right;
+
+    [SerializeField] float speed = 20f;
+    [SerializeField] float turningSpeed = 10f;
+
+    private void OnEnable()
+    {
+        gas.Enable();
+        left.Enable();
+        right.Enable();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (gas.IsPressed())
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (left.IsPressed())
+        {
+            transform.Rotate(Vector3.down * turningSpeed * Time.deltaTime);
+        }
+        if (right.IsPressed())
+        {
+            transform.Rotate(Vector3.up * turningSpeed * Time.deltaTime);
+        }
     }
 }
